f8ee3d0728eefaf7e7c6d78e9c56e4c2c335c9db
sisipho21 <tkssis001@myuct.ac.za>
2022-10-07 19:32:16 +0200

    Question 1 can read in integer value

--- question1.asm
+++ question1.asm
@@ -1,13 +1,36 @@
 .data
+	output: .ascii "The values are:\n"
+	random: .space 20
 	prompt: .ascii "Enter n, followed by n lines of text:\n"	#Prompt string to user
-	num: .space 5    						#Allocating 5 bytes to num for the number
-
+	
+	
 .text #coding section
 
 .globl main
 
 
 main:
-	la $a0, num	#load address of num in $a0
+	# PRINTING OUT PROMPT STRING#
+	li $v0, 4 	#load service 4 (print string)
+	la $a0, prompt	#String to print is 'prompt'
+	syscall
+	
+	# READING IN INTEGER #
 	li $v0, 5 	#load service 5 (read integer)
-	li $a1, 3	#Max number of characters to read
+	syscall
+	
+	move $s0, $v0	#move integer in $v0 into $s0
+	
+	# PRINTING OUT OUTPUT STRING #
+	la $a0, output	#string to print is 'output'
+	li $v0, 4	#load sevice 1 (print string)
+	syscall
+	
+	# PRINTING OUT INTEGER #
+	move $a0, $s0 	#mave integer from $s0 into $a0 for printing
+	li $v0, 1	#load sevice 1 (print integer)
+	syscall 
+	
+exit:
+	li $v0, 10	#load service 10 (exit programme)
+	syscall
